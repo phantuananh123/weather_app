fdf995d28057223b8ee5c24aaf85fc4b7eda479e
phantuananh123 <phanduongtuananh123@gmail.com>
2024-06-06 19:48:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api ={
+    key:"044ca75faa135f595cb237142c293253",
+    base: "https://api.openweathermap.org/data/2.5/",
 }
 
-export default App;
+function App() {
+    const [searchInput, setSearchInput] = useState("")
+    const [searchCity, setSearchCity] = useState("")
+    const [weatherInfor, setWeatherInfor] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [errorMessage, setErrorMessage] = useState(false)
+
+    const handlerSubmit =(e)=>{
+        e.preventDefault();
+        setSearchCity(searchInput);
+    };
+    useEffect(() => {
+        const fetchWeatherData = async () =>    {
+            if(!searchCity)return;
+            setLoading(true)
+            // Process
+            try {
+                const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`
+                const response =  await fetch(url);
+                const data = await response.json();
+                if(response.ok){
+                    setWeatherInfor(`${data.name},${data.sys.country} ${data.weather[0].description},${data.main.temp}`)
+                    setErrorMessage("")
+                }else{
+                    setErrorMessage(data.message)
+                }
+            } catch (error) {
+                setErrorMessage(error.message)
+            }
+            setLoading(false)
+        }
+        fetchWeatherData();
+    }, [searchCity])
+    
+  return (
+    <>
+    <form onSubmit={handlerSubmit}>
+        <input type='text' placeholder='City'value={searchInput} onChange={(e) => setSearchInput(e.target.value)}/>
+        <button>Search</button>
+    </form>
+    {loading ? (<div>Loading...</div>) : (<>{errorMessage ? (<div style={{color:"red"}}>{errorMessage}</div>) : (<div>{weatherInfor}</div>)} </>)}
+    </>
+  )
+}
+
+export default App
